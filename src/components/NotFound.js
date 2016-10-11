ce25057ed66376df908f65fe8a69c717fa2d1145
Sai Grandhi <sgrndh@gmail.com>
2016-10-11 13:05:25 -0400

    :sparkles: Added Router and NotFound compenent

---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+var divStyle = {margin: 40};
+class NotFound extends React.Component {
+    render() {
+        return (
+        	<div style={divStyle}>
+        		<h2>404: Page Not Found</h2>
+        	</div>
+
+        )
+    }
+}
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {BrowserRouter, Match, Miss} from 'react-router';
+
 import './css/style.css';
 import App from './components/App';
 
 import StorePicker from './components/StorePicker';
-render(<App/>, document.querySelector('#main'));
+import NotFound from './components/NotFound';
+// everything in React is a component.
+// Even the router
+const Root = () => {
+	// Wrapping a div around the two Match tags as
+	// JSx does not accept more than one tag
+	// https://facebook.github.io/react/tips/maximum-number-of-jsx-root-nodes.html
+	return (
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={StorePicker} />
+				<Match pattern="/store/:storeId" component={App} />
+				<Miss component={NotFound} />
+			</div>
+		</BrowserRouter>
+	)
+}
+render(<Root/>, document.querySelector('#main'));
