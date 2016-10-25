18c01e0b330c5c8c57cd076f4db86dc96111f6cc
Sai Grandhi <sgrndh@gmail.com>
2016-10-25 00:02:18 -0400

    :sparkles: Added state and updated the Menu and Inventory section

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,64 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+	constructor() {
+		super();
+		// bind the addFish method to this class
+		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
+		// in the initial state
+		// the fishes state is gonna be empty
+		// getInitialState in the normal JavaScript. (this is ES6)
+		this.state = {
+			fishes: {},
+			order: {}
+		};
+	}
+
+	addFish(fish){
+		// first take a copy of the state
+		// then update our state
+		// so ... is called Spread syntax
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_operator
+		const fishes = {...this.state.fishes};
+		// add in our new Fish.
+		// gonna use a timestamp to store fishes
+		const timestamp = Date.now();
+		fishes[`fish-${timestamp}`] = fish;
+		// set state
+		this.setState({ fishes });
+	}
+
+	loadSamples() {
+		this.setState({
+			fishes: sampleFishes
+		})
+	}
+	// whenver we put {} in JSx it means we are
+	// telling react.js that we are going to be using
+	// JavaScript over here.
 	render() {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood" />
+					<ul className="list-of-fishes">
+						{
+							Object
+								.keys(this.state.fishes)
+								.map(key => <Fish key={key} details={this
+									.state.fishes[key]}/>)
+						}
+					</ul>
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
 			</div>
-	)
+		)
 	}
 }
 
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
+    // although the button for loading sample fishes is here
+    // we have to create the loadSamples function where our state is created
+    // because that's how the data from the sample data gets passed to the
+    // components that use the data
     render() {
         return (
-        	<header className="top">
-        		<h1>
-        			Inventory
-        		</h1>
-        	</header>
+        	<div>
+        		<h2>Inventory</h2>
+        		<AddFishForm addFish={this.props.addFish}/>
+                <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
+        	</div>
         )
     }
 }
